fcf03c656d19ff689985bf29b53337c78374e1c4
Ghulam Ishaq <g.ishaq2007@gmail.com>
2025-09-13 03:55:58 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// document.addEventListener('contextmenu', function(e) {
+//   e.preventDefault();
+// });
+
+// // Common shortcuts
+// document.addEventListener('keydown', function(e) {
+//   // F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
+//   if (e.key === 'F12' ||
+//       (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
+//       (e.ctrlKey && (e.key === 'u' || e.key === 'U'))) {
+//     e.preventDefault();
+//     // alert('Inspecting disabled.');
+//   }
+// });
